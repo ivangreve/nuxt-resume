fa76b3e19cef00c4c10792b8fec8364a4c650fc1
Ivan Greve <ivangreve@gmail.com>
2020-06-23 10:02:51 -0300

    json data

--- components/Sections/CardProject.vue
+++ components/Sections/CardProject.vue
@@ -4,7 +4,7 @@
       class="card shadow mb-5 bg-white rounded"
       style="width: 80%; text-align: center"
     >
-      <a href="https://kelawar.com/" class="">
+      <a :href="projectInfo.siteUrl" >
         <img class="card-img-top" :src="projectInfo.image" alt="Kelawar"
       /></a>
       <div class="card-body">
